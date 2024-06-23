54555dcb5a1aeb3412ad7592371f2f6cd268a8b5
address0 <soyong6624@gmail.com>
2024-06-23 22:51:13 +0900

    Feat: Tour Data 조회

--- createDB/urls.py
+++ createDB/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+  path('save-tour-12/', views.save_tour_12, name='save_tour'),
 ]
--- createDB/views.py
+++ createDB/views.py
@@ -12,3 +12,21 @@ from django.shortcuts import get_object_or_404, get_list_or_404
 # Create your views here.
 
 api_key = settings.API_KEY
+BASE_URL = 'https://apis.data.go.kr/B551011/KorService1/areaBasedList1'
+ # ?serviceKey=api_key&contentTypeId=28&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TestApp&_type=json
+
+@api_view(['GET'])
+def save_tour_12(request):
+  URL = BASE_URL
+  params = {
+    'serviceKey': api_key,
+    'contentTypeId': 12,
+    'numOfRows': 100, 
+    'pageNo': 1, 
+    'MobileOS': 'ETC',
+    'MobileApp': 'TestApp',
+    '_type': 'json'
+  }
+  response = requests.get(URL, params=params).json()
+  tour_data = response.get("response").get("body").get("items").get("item")
+  return Response(tour_data)
