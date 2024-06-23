93cd12b3ff90e94878edad98e15e784d88847ec0
address0 <soyong6624@gmail.com>
2024-06-23 20:39:39 +0900

    Feat: 관광지 데이터 model 작성

--- createDB/models.py
+++ createDB/models.py
@@ -18,3 +18,21 @@ class Group_Members(models.Model):
 class Routes(models.Model):
   group = models.ForeignKey(Groups, on_delete=models.CASCADE)
   route_name = models.CharField(max_length=20)
+
+# 국문관광지 데이터
+class Tours(models.Model):
+  sigungucode = models.IntegerField()
+  addr1 = models.TextField()
+  addr2 = models.TextField()
+  image = models.TextField(null=True)
+  cat1 = models.CharField(max_length=3)
+  cat2 = models.CharField(max_length=5)
+  cat3 = models.CharField(max_length=9)
+  type_id = models.IntegerField()
+  mapx = models.FloatField()
+  mapy = models.FloatField()
+  title = models.TextField(null=True)
+  zipcode = models.IntegerField(null=True)
+  tel = models.TextField(null=True)
+  eventstartdate = models.DateTimeField(null=True)
+  eventenddate = models.DateTimeField(null=True)
