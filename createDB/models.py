f202db2787ed75153a286f555f278e18d25795c3
address0 <soyong6624@gmail.com>
2024-06-23 20:16:36 +0900

    Feat: Route model 작성

--- createDB/models.py
+++ createDB/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 # Create your models here.
-class Group(models.Model):
+class Groups(models.Model):
   users = models.ManyToManyField(
     settings.AUTH_USER_MODEL, related_name='joined_group', through='Group_Members'
     )
@@ -13,4 +13,8 @@ class Group(models.Model):
 # 지금 상황에서는 중개 테이블이 필요 없지만, 나중에 추가 정보 저장 가능성을 위해 제작
 class Group_Members(models.Model):
   users = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-  group = models.ForeignKey(Group, on_delete=models.CASCADE)
+  group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+
+class Routes(models.Model):
+  group = models.ForeignKey(Groups, on_delete=models.CASCADE)
+  route_name = models.CharField(max_length=20)
