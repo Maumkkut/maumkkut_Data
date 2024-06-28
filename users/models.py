e4cb29e83a58c29862dcb76d98a289a26e07b712
address0 <soyong6624@gmail.com>
2024-06-28 23:51:50 +0900

    Feat: User model 키워드 가중치, 관광루트 추가

--- users/models.py
+++ users/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from createDB.models import Routes
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    user_tour_type = models.TextField(null=True)
+    user_healing = models.IntegerField(null=True)
+    user_relax = models.IntegerField(null=True)
+    user_nature = models.IntegerField(null=True)
+    user_exhibit = models.IntegerField(null=True)
+    user_food = models.IntegerField(null=True)
+    user_adventure = models.IntegerField(null=True)
+    user_people = models.IntegerField(null=True)
+    user_shopping = models.IntegerField(null=True)
+    user_photo = models.IntegerField(null=True)
+    route = models.ForeignKey(Routes, on_delete=models.SET_NULL, null=True)
 
 
