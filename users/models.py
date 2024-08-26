0a58d18cef4e7aaf977a338c1e43eb180eee755c
address0 <soyong6624@gmail.com>
2024-08-26 22:21:19 +0900

    [DB] feat: user DB 분리

--- users/models.py
+++ users/models.py
@@ -4,18 +4,6 @@ from createDB.models import Tours
 
 # Create your models here.
 class User(AbstractUser):
-    user_age = models.IntegerField(null=True)
-    user_type = models.TextField(null=True)
-    user_healing = models.IntegerField(null=True)
-    user_relax = models.IntegerField(null=True)
-    user_nature = models.IntegerField(null=True)
-    user_exhibit = models.IntegerField(null=True)
-    user_food = models.IntegerField(null=True)
-    user_adventure = models.IntegerField(null=True)
-    user_people = models.IntegerField(null=True)
-    user_shopping = models.IntegerField(null=True)
-    user_photo = models.IntegerField(null=True)
-    tour_like = models.ManyToManyField(Tours, symmetrical=False, related_name='liked_users')
-    tour_dislike = models.ManyToManyField(Tours, symmetrical=False, related_name='disliked_users')
+    pass
 
 
